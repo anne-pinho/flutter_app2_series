c68e3ef270ff41cdf0f52208b9e6c751c265e6a9
anne-pinho <anne.pinhov@gmail.com>
2025-07-31 16:40:04 -0400

    Aula 10/07: add changes themes and rating widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app2_series/tv_show_card.dart';
 import 'package:flutter_app2_series/tv_show_data.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,14 +12,72 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var color = Colors.purple;
+    var colorScheme = ColorScheme.fromSeed(
+      seedColor: color,
+      brightness: Brightness.light,
+    );
+
+    var colorSchemeDark = ColorScheme.fromSeed(
+      seedColor: color,
+      brightness: Brightness.dark,
+    );
+
+    var customTheme = ThemeData(
+      colorScheme: colorScheme,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        toolbarHeight: 100,
+        backgroundColor: colorScheme.primary,
+        titleTextStyle: GoogleFonts.nunito(
+          fontSize: 36,
+          fontWeight: FontWeight.bold,
+          color: colorScheme.onPrimary,
+        ),
+      ),
+      cardTheme: CardThemeData(
+        color: colorScheme.secondaryContainer,
+        shadowColor: colorScheme.onSurface,
+        elevation: 5,
+        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      ),
+    );
+
+    var customThemeDark = ThemeData(
+      colorScheme: colorSchemeDark,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        toolbarHeight: 100,
+        backgroundColor: colorScheme.primary,
+        titleTextStyle: GoogleFonts.nunito(
+          fontSize: 36,
+          fontWeight: FontWeight.bold,
+          color: colorScheme.onPrimary,
+        ),
+      ),
+      cardTheme: CardThemeData(
+        color: colorScheme.onSecondaryContainer,
+        shadowColor: colorScheme.onSurface,
+        elevation: 5,
+        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      ),
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: customTheme,
+      darkTheme: customThemeDark,
+      themeMode: ThemeMode.system,
       home: Scaffold(
         appBar: AppBar(title: const Text('Eu Amo Séries 🎬')),
-        body: ListView(
-          children: [
-            ...favTvShowList.map((tvShow) => TvShowCard(tvShow: tvShow)),
-          ],
+        body: ListView.builder(
+          itemCount: favTvShowList.length,
+          itemBuilder: (context, index) =>
+              TvShowCard(tvShow: favTvShowList[index], index: index),
         ),
       ),
     );
